7baffcda544f756511753d5ddb54f813d033489f
Death916 <tavn1992@gmail.com>
2018-05-07 20:08:20 -0700

    working on getting right message

--- getsignal.py
+++ getsignal.py
@@ -6,8 +6,29 @@ import pyzmail
 imap = imapclient.IMAPClient('imap.gmail.com', ssl=True)
 imap.login('tavn1992@gmail.com', 'tzzjgbzdtadlflls')
 imap.select_folder('CRYPTO/trade', readonly=True)
-msg = imap.fetch([3], [b'BODY[]', b'FLAGS'])
 
-message = pyzmail.PyzMessage.factory(msg[3][b'BODY[]'])
 
-print(message.get_subject())
+alert = imap.search(b'SEEN')
+
+for num in range(0, 100000):
+	if num in alert:
+		uid = num
+
+
+msg = imap.fetch([uid], [b'BODY[]', b'FLAGS'])
+
+
+message = pyzmail.PyzMessage.factory(msg[uid][b'BODY[]'])
+
+if 'strategy says sell now' in message.get_subject():
+	print('sell signal found')
+	print(message.get_subject())
+elif 'strategy says buy now' in message.get_subject():
+	print('buy signal found')
+	print(message.get_subject())
+
+
+else:
+
+	print('failed')
+
