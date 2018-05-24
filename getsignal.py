be318e7e1b303fc7a3ad03716f1bfae0e7dd512a
Death916 <tavn1992@gmail.com>
2018-05-24 01:01:49 -0700

    start buy and sell functions

--- getsignal.py
+++ getsignal.py
@@ -7,7 +7,6 @@ import glogin
 #login to server
 imap = glogin.connect()
 imap.select_folder('CRYPTO/trade', readonly=True)
-
 alert = imap.search(b'UNSEEN')
 
 #get all unseen uids
@@ -15,28 +14,36 @@ for num in range(0, 100000):
 	if num in alert:
 		uid = num
 
-
 msg = imap.fetch([uid], [b'BODY[]', b'FLAGS'])
-
-
 message = pyzmail.PyzMessage.factory(msg[uid][b'BODY[]'])
+
 trade = None
+
+def start_buy():
+	if trade == "buy":
+		print('trade is a buy')
+
+def start_sell():
+	if trade == "sell":
+		print('trade is a sell')
+
+
+
 if 'strategy says sell now' in message.get_subject():
-	print('sell signal found')
-	print(message.get_subject())
-	trade = sell
+		print('sell signal found')
+		print(message.get_subject())
+		trade = "sell"
+
 elif 'strategy says buy now' in message.get_subject():
 	print('buy signal found')
 	print(message.get_subject())
-	trade = buy
-
+	trade = "buy"
 
 else:
 
 	print('failed')
 
-#class start_trade():
 
 
-
-#
+#start_buy()
+#start_sell()
