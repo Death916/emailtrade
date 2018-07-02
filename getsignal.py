93e881c8f427381f482960380a3ed1b47d2f528f
Death916 <tavn1992@gmail.com>
2018-07-02 00:20:33 -0700

    cleanup

--- getsignal.py
+++ getsignal.py
@@ -1,12 +1,12 @@
 #! python3
 
-import imapclient
 import pyzmail
 import glogin
-import subprocess
-import time
-import psutil
 import trade
+import history as hist
+import psutil
+import time
+
 
 # login to server
 
@@ -14,22 +14,25 @@ def gconnect():
 	imap = glogin.connect()
 	imap.select_folder('CRYPTO/trade', readonly=True)
 	alert = imap.search(b'UNSEEN')
-	global uid
-	# get all unseen uids
-	for num in range(0, 100000):
-		if num in alert:
-			uid = num
+	try:
+		global uid
+		# get all unseen uids
+		for num in range(0, 100000):
+			if num in alert:
+				uid = num
 
-	msg = imap.fetch([uid], [b'BODY[]', b'FLAGS'])
-	global message
-	message = pyzmail.PyzMessage.factory(msg[uid][b'BODY[]'])
-	print('connected')
+		msg = imap.fetch([uid], [b'BODY[]', b'FLAGS'])
+		global message
+		message = pyzmail.PyzMessage.factory(msg[uid][b'BODY[]'])
+		print('connected')
+	except NameError:
+		print('NO new trade')
+
 
 last_alert = 0
 
 
 def kill(proc_pid):
-	
 	process = psutil.Process(proc_pid)
 	for procs in process.children(recursive=True):
 		procs.kill()
@@ -37,24 +40,24 @@ def kill(proc_pid):
 
 
 def start_buy():
-	
 	print('trade is a buy')
-	trade.open_trade(1)
+	trade.open_trade(.0005)
 	global last_alert
 	last_alert = "buy"
-	
-	
-def start_sell():
+	hist.tradehist('buy test')
 
+
+def start_sell():
 	print('trade is a sell')
-	trade.close_trade(1)
+	trade.close_trade(0)
 	global last_alert
 	last_alert = "sell"
-	
+	hist.tradehist('sell test')
+
 
 def get_signal():
-	try: 
-		if 'strategy says sell now' in message.get_subject() or last_alert == 'buy':	
+	try:
+		if last_alert == 'buy' and last_uid != uid:
 			print('sell signal found')
 			coin = message.get_subject()
 			print(coin)
@@ -63,28 +66,28 @@ def get_signal():
 		elif last_alert == 'sell' or last_alert == 0:
 			print('buy signal found')
 			coin = message.get_subject()
-			"""if 'ETHBTC' in coin:     # TODO check for current ticker to use
-				print('true')
-			else:
-    				print('false') """
+
 			print(coin)
-			
+
 			return "buy"
-			
+
 	except:
 		print('failed')
 
 
 last_uid = 0
 
+
 def main():
 	while True:
-
-		global uid
-		gconnect()
-		print(uid)
-		signal = get_signal()
-		print(time.ctime())
+		try:
+			global uid
+			gconnect()
+			print(uid)
+			signal = get_signal()
+			print(time.ctime())
+		except NameError:
+			print('no trade')
 
 		try:
 			global last_uid
@@ -92,21 +95,20 @@ def main():
 
 				if signal == "buy" and last_alert != 'buy':
 					start_buy()
-					print(last_alert)
+					print('last alert was', last_alert)
 					last_uid = uid
 				elif signal == "sell" and last_alert == "buy":
 					start_sell()
-					print(last_alert)
+					print('last alert was ', last_alert)
 					last_uid = uid
 			else:
 				print('same uid')
 		except:
-			print(' fail')
+			print(' trade fail')
 		print(trade.open_orders())
-		
+
 		time.sleep(600)
 
-if __name__ == '__main__':
-    	main()
-	
 
+if __name__ == '__main__':
+	main()
