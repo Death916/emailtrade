214bcd1f48c09c473957247dcdc249a9d0335818
Death916 <tavn1992@gmail.com>
2018-07-10 00:56:47 -0700

    formatting

--- trade.py
+++ trade.py
@@ -1,8 +1,8 @@
 #! python3
 
 import json
-from bittrex.bittrex import Bittrex, API_V2_0 
-from bittrex import Bittrex as trex1
+from bittrex import Bittrex
+
 import history as hist
 
 amount = 0
@@ -14,37 +14,39 @@ with open('D:/code/emailtrade/keys.json') as k:
 api_key = keys['api_key']
 priv_key = keys['priv_key']
 ticker = 'BTC-ETH'
-trex = Bittrex(api_key, priv_key, api_version=API_V2_0)
+trex = Bittrex(api_key, priv_key)
 
 
 def marketcheck(ticker):
-    markets = trex.get_latest_candle(ticker, 'fiveMin')
-    #price = markets['result']['Ask']
-    print(markets)
+    markets = trex.get_ticker(ticker)
+    price = markets['result']['Ask']
+    #print(markets)
     return price
 
 
 def open_trade(amount):
     global buyprice
     buyprice = marketcheck(ticker)
-    trex.buy_limit(ticker, amount, rate = buyprice)
+    #print(trex.buy_limit(ticker, amount, rate=price))
     print('buying',  amount, 'of', ticker)
-    hist.tradehist('bought ' +  amount + ' of ' + ticker + ' at ' + str(buyprice))
+    hist.tradehist('bought ' + str(amount) + ' of ' + ticker + ' at ' + str(buyprice))
     print(buyprice)
+    return
 
 def close_trade(amount):
     global sellprice
     sellprice = marketcheck(ticker)
-    trex.sell_limit(ticker, amount, rate = None)
+    #print(trex.sell_limit(ticker, amount, rate = None))
     print('selling',  amount, 'of', ticker)
-    hist.tradehist('sold ' +  amount + ' of ' + ticker + ' at ' + str(sellprice))
-    hist.tradehist('profit = ' + str(sellprice - buyprice))
+    hist.tradehist('sold ' + str(amount) + ' of ' + ticker + ' at ' + str(sellprice))
+    hist.tradehist('profit = ' + '{:.20f}'.format((sellprice * amount) - (buyprice * amount)))
 
 def open_orders():
     trex.get_open_orders()
+    return
 
     
 
 #open_trade('.00051')
 #close_trade('1')
-marketcheck(ticker)
+#marketcheck(ticker)
