3306274acf8681175616b313e5d34c97da0d95f6
Death916 <tavn1992@gmail.com>
2018-07-27 01:22:58 -0700

    error catching

--- getsignal.py
+++ getsignal.py
@@ -1,6 +1,4 @@
-#! python3
-
-
+import pyzmail
 import glogin
 import trade
 import history as hist
@@ -11,117 +9,100 @@ import time
 # login to server
 
 def gconnect():
-	imap = glogin.connect()
-	imap.select_folder('CRYPTO/trade', readonly=True)
-	return imap
-
+    imap = glogin.connect()
+    imap.select_folder('CRYPTO/trade', readonly=True)
+    return imap
 
 last_alert = 0
 
-
 def start_buy():
-	print('trade is a buy')
-	trade.open_trade(1)
-	global last_alert
-	last_alert = "buy"
-	hist.tradehist('buy test')
+    print('trade is a buy')
+    trade.open_trade(1)
+    global last_alert
+    last_alert = "buy"
+    hist.tradehist('buy test')
 
 
-def start_sell():
-	print('trade is a sell')
-	trade.close_trade(1)
-	global last_alert
-	last_alert = "sell"
-	hist.tradehist('sell test')
+def start_sell(): 
+    
+    print('trade is a sell')
+    trade.close_trade(1)
+    global last_alert
+    last_alert = "sell"
+    hist.tradehist('sell test')
 
 
-def get_signal():
-	# noinspection PyPep8
-	try:
-		global uid
-		if last_alert == 'buy' and last_uid != uid:
-			print('sell signal found')
-			signal = 'sell'
-			return signal
+def getsignal():
+    try:
+        global uid
+        if last_alert == 'buy' and last_uid != uid:
+            print('sell signal found')
+            return "sell"
 
-		elif last_alert == 'sell' or last_alert == 0:
-			print('buy signal found')
-			# coin = message.get_subject()
-			signal = 'buy'
-			return signal
+        elif last_alert == 'sell' or last_alert == 0:
+            print('buy signal found')
+            return"buy"
 
-	except:
-		print('get_signal() failed')
+    except:
+        print('failed')
 
 
 last_uid = 0
 
 
 def main():
-	s = gconnect()
+    s = gconnect()
+    idle = s
+    s.idle()
+    start_time = time.time()
+    while True:
 
-	s.idle()
-	start_time = time.time()
-	while True:
-
-		responses = s.idle_check(30)
-		print("Server sent:", responses if responses else "nothing")
-
-		list_uid = ([i[0] for i in responses])
-		global last_uid
-		if list_uid:
-			global uid
-			print(list_uid[0])
-			uid = list_uid[0]
-			signal = get_signal()
-			global last_uid
-			if last_uid != uid:
-
-				if signal == "buy" and last_alert != 'buy':
-					start_buy()
-					print('last alert was', last_alert)
-					last_uid = uid
-				elif signal == "sell" and last_alert == "buy":
-					start_sell()
-					print('last alert was ', last_alert)
-					last_uid = uid
-
-		## for debugging only ## TODO: Remove when done
-
-		elif last_uid == 0:
-			uid = 0
-			signal = get_signal()
-
-			if signal == "buy" and last_alert != 'buy':
-				start_buy()
-				print('last alert was', last_alert)
-				last_uid = uid
-			elif signal == "sell" and last_alert == "buy":
-				start_sell()
-				print('last alert was ', last_alert)
-				last_uid = uid
-		##                  ##
-		else:
-			print('same uid')
+        responses = idle.idle_check(30)
+        print("Server sent:", responses if responses else "nothing")
 
 
-		print(time.ctime())
+        list_uid = ([i[0] for i in responses])
+        if list_uid != []:
+            global uid
+            print(list_uid[0])
+            uid = list_uid[0]
+            signal = getsignal()
+        else:
+            uid = 0
+
+        print(time.ctime())
+        print('last alert was ', last_alert)
 
 
+        global last_uid
+
+        if last_uid != uid:
+
+            if signal == "buy" and last_alert != 'buy':
+                start_buy()
+                print('last alert was', last_alert)
+                last_uid = uid
+            elif signal == "sell" and last_alert == "buy":
+                start_sell()
+                print('last alert was ', last_alert)
+                last_uid = uid
+        else:
+            print('same uid')
+
+        if trade.open_orders() != None:
+            print(trade.open_orders())
+        if time.time() - start_time > 1740:
+            try:
+                s.idle_done()
+                print('restarting connection')
+                s = gconnect()
+                s.idle()
+                start_time = time.time()
+            except:
+                print('restart failed')
 
 
-
-		if trade.open_orders():
-			print(trade.open_orders())
-
-		if time.time() - start_time > 1740:
-			s.idle_done()
-			print('restarting connection')
-			s.logout()
-			s = gconnect()
-			s.idle()
-			start_time = time.time()
 
 
 if __name__ == '__main__':
-	main()
+    main()
